55fd8bc68107523caa07d6221493106b5684a35f
AnastasiiaSteblyna <zanegina93@gmail.com>
2022-11-29 16:13:55 +0200

    -

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,26 @@
-import user from './Profile/user.json';
 import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionsHistory } from './TransactionHistory/TransactionsHistory';
+
+import user from './data/user.json';
+import data from './data/data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionsHistory items={transactions} />
+    </>
   );
 };
---
+++ src/components/TransactionHistory/TransactionsHistory.js
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import { TransactionItem } from './TransactionItem';
+
+export const TransactionsHistory = ({ items }) => {
+  return (
+    <table class="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <TransactionItem
+            key={id}
+            type={type}
+            amount={amount}
+            currency={currency}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionsHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
